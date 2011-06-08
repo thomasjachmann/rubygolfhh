c2422961afdff1fce344083b68f407a3c9e576b3
Thomas Jachmann <self@thomasjachmann.com>
2011-06-08 19:53:24 +0200

    hole3 and shortening of hole1

--- lib/golf.rb
+++ lib/golf.rb
@@ -1,7 +1,17 @@
 class Golf
 
-  def self.hole1(args)
-    args.inject(1) {|i,m| i*m}
+  def self.hole1(a)
+    a.inject(1) {|i,m| i * m}
+  end
+  
+  def self.hole3(s)
+    s.downcase!
+    l = s.length
+    a = l/2
+    a -= 1 if l % 2 == 0
+    a = s[0..(a)]
+    b = s[(l/2)..-1]
+    a == b.reverse
   end
 
 end
